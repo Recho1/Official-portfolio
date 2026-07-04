6db739a597220119994006fd63c1c6b9ef043a93
Recho1 <rachealacio501@gmail.com>
2026-07-04 08:43:16 +0300

    fix: add MdEmail import to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { MdEmail } from 'react-icons/md';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
